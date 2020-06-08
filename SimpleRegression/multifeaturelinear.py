10d7af24e72c3ea4dbbf0a5ce27782e47793530a
Shashank <shashankbhor@gmail.com>
2020-06-08 10:39:54 -0400

    Added prog for p value verification
    p Value is significance of null hypothesis. Higher the value indicates null hypothersis is true means there is no co-releation between dependant veriable and in-dependant veriable

--- SimpleRegression/multifeaturelinear.py
+++ SimpleRegression/multifeaturelinear.py
@@ -10,12 +10,15 @@ and keep only co-related features for model building
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pandas import DataFrame
 
 # dataset collection
 dataset=pd.read_csv('50_Startups.csv')
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,4].values
 
+X = DataFrame(X,columns=['R&D Spend','Administration','Marketing Spend','State']) 
+X  = np.array(X)
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 lableencoder_X=LabelEncoder()
@@ -48,15 +51,16 @@ y_pred = regressor.predict(X_test)
 import statsmodels.api as sm
 # Adding first column as '1' to ensure b0*X0 =b0 (of b0X0 + b1X1 + b2X2 ..)
 X=np.append(arr=np.ones((50,1)).astype(int), values=X, axis=1)
+
 X_opt=X[:,[0,1,2,3,4,5]]
 regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
 regressor_OLS.summary()
 # Above execution gave 99% P-value for X2 (i.e. 3th column) so removing it for next iteration
-X_opt=X[:,[0,1,2,3,5]]
+X_opt=X[:,[0,1,3,4,5]]
 regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
 regressor_OLS.summary()
 # removing 2
-X_opt=X[:,[0,1,2,3,5]]
+X_opt=X[:,[0,3,4,5]]
 regressor_OLS=sm.OLS(endog=y,exog=X_opt).fit()
 regressor_OLS.summary()
 #Removing 4
